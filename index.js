1c1e06dd32be428b2ac2ecfa6e94200b8b47f07f
LeeJuYoung <ken44929@gmail.com>
2020-10-27 19:08:41 +0900

    로그인 기능

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ app.post('/register', (req, res) => {
    });
 });
 
+app.post('/login', (req, res) => {});
+
 app.listen(port, () => {
    console.log(`Example app listening at http://localhost:${port}!`);
 });
--- models/User.js
+++ models/User.js
@@ -1,6 +1,10 @@
 // 1. mongoose 호출
 const mongoose = require('mongoose');
 
+// 비밀번호 암호화(bcrypt)
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 // 2. Model(=Table) 생성
 const userSchema = mongoose.Schema({
    name: {
@@ -39,6 +43,28 @@ const userSchema = mongoose.Schema({
    tokenExp: {
       type: Number,
    },
+});
+
+//유저 정보를 저장하기 전에
+userSchema.pre('save', function (next) {
+   let user = this;
+   // 비밀번호를 암호화 시킨다.
+
+   // isModified => '비밀번호'가 변경될 때만 암호화 해준다.
+   // 이메일, 이름 등이 바뀔 때는 암호화 하지 않음.
+   if (user.isModified('password')) {
+      bcrypt.genSalt(saltRounds, function (err, salt) {
+         if (err) return next(err);
+
+         bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+         });
+      });
+   } else {
+      next();
+   }
 });
 
 const User = mongoose.model('User', userSchema);
