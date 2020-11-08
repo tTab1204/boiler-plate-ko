dad4786a309f166a90e4414eb799aa96efbd601c
LeeJuYoung <ken44929@gmail.com>
2020-11-08 18:21:19 +0900

    인증기능

--- index.js
+++ index.js
@@ -92,6 +92,15 @@ app.get('/api/users/auth', auth, (req, res) => {
    });
 });
 
+app.get('/api/users/logout', auth, (res, req) => {
+   User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.json(200).send({
+         success: true,
+      });
+   });
+});
+
 app.listen(port, () => {
    console.log(`Example app listening at http://localhost:${port}!`);
 });
